f853561978a1afe274e6d0e2f7b30ad704fca17b
nodejh <replyjh@gmail.com>
2016-04-18 18:43:54 +0800

    Express的基本使用

---
+++ class/nodejh/研究开发与实践/第七周作业/menu_2/routes/index.js
@@ -0,0 +1,48 @@
+module.exports = function(app) {
+  app.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+  });
+
+
+  app.post('/menu', function (req, res) {
+
+    var type = req.body.type;
+    console.log(type);
+    req.getConnection(function(err, connection) {
+
+      if (err) {
+        console.log(err);
+        res.json({code: 1, msg: err});
+        return false;
+      }
+
+      var select = '';
+      switch (type) {
+        case 'not_login':
+          select = 'SELECT `name` FROM `not_login_menu`';
+          break;
+        case 'login':
+          select = 'SELECT `name` FROM `login_menu`';
+          break;
+        default:
+          return res.json({code: 2, msg: 'wrong type'});
+      }
+
+      connection.query(select, function (err, raws) {
+
+        if (err) {
+          res.json({code: 3, msg: err});
+          return false;
+        }
+
+        console.log('get menu: ', raws);
+        res.json({
+          code: 0,
+          menu: raws
+        });
+      });
+
+    });
+  });
+
+};
